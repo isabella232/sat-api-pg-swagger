1d994dc4140640fd280f03ffc677e7446ca57690
Sean Harkins <sharkinsgis@gmail.com>
2019-10-28 15:38:40 -0400

    first

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import './index.css';
+// import App from './App';
 import * as serviceWorker from './serviceWorker';
-
-ReactDOM.render(<App />, document.getElementById('root'));
+import SwaggerUI from 'swagger-ui';
+import 'swagger-ui/dist/swagger-ui.css';
+SwaggerUI({
+  dom_id: '#root',
+  url: 'https://raw.githubusercontent.com/radiantearth/stac-spec/master/api-spec/STAC.yaml'
+})
+// ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
